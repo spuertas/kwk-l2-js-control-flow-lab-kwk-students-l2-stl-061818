f0ab8ae543a393ee070ea59831b41383a22bc7ef
Sophia <spuertas@micds.org>
2018-06-22 14:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
-  if ()
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!"
+  }
+  else {
+    //don't give free sample
+  }
 }
 function ternaryCheckCity() {
   
