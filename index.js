2fa0b76a176628307bba33ed166f8cfb149ef9b9
Sophia <spuertas@micds.org>
2018-06-22 14:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,18 @@ function ternaryCheckCity(city) {
   return location;
 }
 function switchOnCharmFromTip() {
-  
+  switch () {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
 }
+
